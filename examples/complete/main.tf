c9da20bf467eaffd1ba3440644f778ac04e49053
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2023-10-12 19:32:30 +0000

    chore(deps): update terraform-module to v1.1.0

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -25,7 +25,7 @@ locals {
 
 module "resource_group" {
   source  = "terraform-ibm-modules/resource-group/ibm"
-  version = "1.0.6"
+  version = "1.1.0"
   # if an existing resource group is not set (null) create a new one using prefix
   resource_group_name          = var.resource_group == null ? "${var.prefix}-resource-group" : null
   existing_resource_group_name = var.resource_group
@@ -54,7 +54,7 @@ resource "ibm_resource_instance" "secrets_manager" {
 
 module "secrets_manager_group" {
   source                   = "terraform-ibm-modules/secrets-manager-secret-group/ibm"
-  version                  = "1.0.1"
+  version                  = "1.1.0"
   region                   = local.sm_region
   secrets_manager_guid     = local.sm_guid
   secret_group_name        = "${var.prefix}-group"
