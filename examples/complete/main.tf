cb58d73fd91d64ccefe477c828cff9f5e50ac3a6
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2025-05-24 08:27:31 +0000

    chore(deps): update terraform terraform-ibm-modules/cos/ibm to v9

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -195,7 +195,7 @@ module "secret_manager_imported_cert" {
 # create a COS instance to create the service credential for
 module "cloud_object_storage" {
   source                             = "terraform-ibm-modules/cos/ibm"
-  version                            = "8.21.25"
+  version                            = "9.0.3"
   resource_group_id                  = module.resource_group.resource_group_id
   region                             = var.region
   cos_instance_name                  = "${var.prefix}-cos"
