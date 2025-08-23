24ab551ed15443cb8eeb9d8380cac4776ca232fa
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2025-08-23 07:14:32 +0000

    chore(deps): update dependencies

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -31,7 +31,7 @@ module "resource_group" {
 module "secrets_manager" {
   count                = var.existing_sm_instance_guid == null ? 1 : 0
   source               = "terraform-ibm-modules/secrets-manager/ibm"
-  version              = "2.7.7"
+  version              = "2.7.8"
   resource_group_id    = module.resource_group.resource_group_id
   region               = local.sm_region
   secrets_manager_name = "${var.prefix}-secrets-manager"
