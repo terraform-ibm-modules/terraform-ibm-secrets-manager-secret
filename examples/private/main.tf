490a6d534943f1a7bef51b82e6be10f485afc8af
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2025-09-05 23:17:14 +0000

    chore(deps): update dependencies

--- examples/private/main.tf
+++ examples/private/main.tf
@@ -28,7 +28,7 @@ module "resource_group" {
 
 module "secrets_manager" {
   source                        = "terraform-ibm-modules/secrets-manager/ibm"
-  version                       = "2.8.4"
+  version                       = "2.8.6"
   existing_sm_instance_crn      = var.existing_sm_instance_crn
   resource_group_id             = module.resource_group.resource_group_id
   region                        = local.sm_region
@@ -200,7 +200,7 @@ module "secret_manager_imported_cert" {
 # create a COS instance to create the service credential for
 module "cloud_object_storage" {
   source                              = "terraform-ibm-modules/cos/ibm"
-  version                             = "10.2.11"
+  version                             = "10.2.13"
   resource_group_id                   = module.resource_group.resource_group_id
   region                              = local.sm_region
   cos_instance_name                   = "${var.prefix}-cos"
